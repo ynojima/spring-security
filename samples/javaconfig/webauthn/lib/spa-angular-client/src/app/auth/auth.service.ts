17a07d17ff5d45822a05e0822782f3c7dd06a695
ynojima <mail@ynojima.net>
2019-05-04 21:22:58 +0900

    Add WebAuthn sample application

---
+++ samples/javaconfig/webauthn/lib/spa-angular-client/src/app/auth/auth.service.ts
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2002-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {base64url} from "rfc4648";
+import {WebAuthnService} from "../webauthn/web-authn.service";
+import {Observable} from "rxjs/internal/Observable";
+import {from} from 'rxjs';
+import {map, concatMap} from 'rxjs/operators';
+import {throwError} from "rxjs/internal/observable/throwError";
+import {WebAuthn4NgCredentialRequestOptions} from "../webauthn/web-authn-4-ng-credential-request-options";
+import {ServerOptions} from "../webauthn/server-options";
+import {AuthResponse} from "./auth-response";
+import {AuthenticationStatus} from "./authentication-status";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+
+  private loginUrl: string = "/login";
+  private logoutUrl: string = "/logout";
+  private authStatusUrl: string = "/api/auth/status";
+
+  constructor(private webauthnService: WebAuthnService, private http: HttpClient) { }
+
+
+  loginWithPublicKeyCredential(credentialRequestOptions: WebAuthn4NgCredentialRequestOptions): Observable<string> {
+    let promise = this.webauthnService.fetchServerOptions().toPromise().then((serverOptions)=>{
+      return this.webauthnService.getCredential(credentialRequestOptions, serverOptions).then(credential => {
+        return {serverOptions: serverOptions, credential: credential}
+      });
+    });
+
+    return from(promise).pipe(concatMap((data) =>{
+      if(data.credential.type != "public-key"){
+        throwError("Unexpected credential type");
+      }
+      let publicKeyCredential: PublicKeyCredential = data.credential as PublicKeyCredential;
+      let assertionResponse: AuthenticatorAssertionResponse = publicKeyCredential.response as AuthenticatorAssertionResponse;
+      let clientDataJSON = assertionResponse.clientDataJSON;
+      let authenticatorData = assertionResponse.authenticatorData;
+      let signature = assertionResponse.signature;
+      // let clientExtensions = publicKeyCredential.getClientExtensionResults(); //Edge preview throws exception as of build 180603-1447
+      let clientExtensions = {};
+
+      if(publicKeyCredential.response as AuthenticatorAttestationResponse){
+        let formData = new FormData();
+        formData.set(data.serverOptions.parameters.credentialId, base64url.stringify(new Uint8Array(publicKeyCredential.rawId)));
+        formData.set(data.serverOptions.parameters.clientDataJSON, base64url.stringify(new Uint8Array(clientDataJSON)));
+        formData.set(data.serverOptions.parameters.authenticatorData, base64url.stringify(new Uint8Array(authenticatorData)));
+        formData.set(data.serverOptions.parameters.signature, base64url.stringify(new Uint8Array(signature)));
+
+        return this.http.post(this.loginUrl, formData, {responseType: 'text'});
+      }
+    }));
+  }
+
+  loginWithPasswordCredential(username: string, password: string): Observable<string> {
+    let data = new FormData();
+    data.set('username', username);
+    data.set('password', password);
+
+    return this.http.post(this.loginUrl, data, {responseType: 'text'});
+  }
+
+  logout(): Observable<string>{
+    return this.http.post(this.logoutUrl, null, {responseType: 'text'});
+  }
+
+  getAuthenticationStatus(): Observable<AuthenticationStatus> {
+    return this.http.get<AuthResponse>(this.authStatusUrl).pipe(map<AuthResponse, AuthenticationStatus>(response => response.status));
+  }
+
+
+}
---
+++ samples/javaconfig/webauthn/spa/src/main/resources/db/migration/mysql/V0_0_1__dataload_tables.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO m_group VALUES (1, 'Group A');
+INSERT INTO m_group VALUES (2, 'Group B');
+INSERT INTO m_group VALUES (3, 'Group C');
+
+INSERT INTO m_authority VALUES (1, 'ROLE_ADMIN');
+INSERT INTO m_authority VALUES (2, 'ROLE_ACTUATOR');
+
+INSERT INTO r_group_authority VALUES (1, 1);
+
+COMMIT;
